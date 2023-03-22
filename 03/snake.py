cedd4afd5cc15a498c436be2b6de98b346a601ae
dehmovlaei <db.dehmovlaei@gmail.com>
2023-03-22 23:27:54 +0430

    fix Snake

--- 03/snake.py
+++ 03/snake.py
@@ -1,4 +1,13 @@
+j = 0
 lenSnake = int(input("enter len of snake\n"))
 for i in range(lenSnake):
     print("*", end = "")
+    j += 1
+    if j == lenSnake:
+        break
     print("#", end = "")
+    j += 1
+    if j == lenSnake:
+        break
+    
+    
