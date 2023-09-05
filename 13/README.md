67e06243212cdb9e2bd79fa190de2df39dfd4c6b
unknown <dehmovlaei@dehmovlaei.TUKAIT.ORG>
2023-09-05 11:00:07 +0330

    09052023:1100

---
+++ 13/README.md
@@ -0,0 +1,10 @@
+# PyLearn Assignments
+## train some course and solved assignments
+
+---
+| File      | Description |
+| ----------- | ----------- |
+|Interstellar|a Mini Game on progress |
+|diamond|Drawing rectangle of diamond with arcade triangle |
+![concentric](diamond.jpg)
+
--- 13/diamond.py
+++ 13/diamond.py
@@ -1,9 +1,9 @@
 import arcade
 
-RAW_COL = 65
-RAW_ROW = 65
-l_margin = 50
-b_margin = 50
+raw_col = 65
+raw_row = 65
+l_margin = 35
+b_margin = 35
 
 arcade.open_window(650, 650, "DIAMOND")
 arcade.set_background_color(arcade.color.WHITE)
@@ -11,13 +11,13 @@ arcade.start_render()
 
 for row in range(10):
     for column in range(10):
-        x = column * RAW_COL + l_margin
-        y = row * RAW_ROW + b_margin
-        if column%2==0 and row%2==0  or column%2==1 and row%2==1:
-            arcade.draw_triangle_filled(x-7,y, x, y+7,x+7,y, arcade.color.RED)
-            arcade.draw_triangle_filled(x-7,y, x, y-7,x+7,y, arcade.color.RED)           
-        if column%2==1 and row%2==0  or column%2==0 and row%2==1:
-            arcade.draw_triangle_filled(x-7,y, x, y+7,x+7,y, arcade.color.BLUE)
-            arcade.draw_triangle_filled(x-7,y, x, y-7,x+7,y, arcade.color.BLUE)
+        x = column * raw_col + l_margin
+        y = row * raw_row + b_margin
+        if column % 2 == 0 and row %2 == 0  or column % 2 == 1 and row %2 == 1:
+            arcade.draw_triangle_filled(x - 10,y, x, y + 10,x + 10,y, arcade.color.RED)
+            arcade.draw_triangle_filled(x - 10,y, x, y - 10,x + 10,y, arcade.color.RED)           
+        if column % 2 == 1 and row % 2 == 0  or column % 2 == 0 and row % 2 == 1:
+            arcade.draw_triangle_filled(x - 10,y, x, y + 10,x + 10,y, arcade.color.BLUE)
+            arcade.draw_triangle_filled(x - 10,y, x, y - 10,x + 10,y, arcade.color.BLUE)
 arcade.finish_render()
 arcade.run()
