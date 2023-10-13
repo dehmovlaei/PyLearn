6ada064af5ace5716d8823b70c052980b2267376
dehmovlaei <db.dehmovlaei@gmail.com>
2023-10-13 20:45:14 +0330

    10132023:2045

--- 15/super_snake.py
+++ 15/super_snake.py
@@ -22,20 +22,29 @@ class Snake(arcade.Sprite):
         self.color = arcade.color.GREEN_YELLOW
         self.change_x = 0
         self.change_y = 0
-        self.speed = 4
+        self.speed = 2
         self.score = 0
+        self.body = []
 
     def draw(self):
         arcade.draw_circle_filled(self.center_x, self.center_y, self.radius, self.color)
+        
+        for part in self.body:
+            arcade.draw_circle_filled(part['x'], part ['y'], self.radius, self.color)
 
     def move(self):
         self.center_x += self.change_x * self.speed
         self.center_y += self.change_y * self.speed
+        self.body.append({'x':self.center_x, 'y':self.center_y})
+        
+        if len(self.body) > self.score:
+            self.body.pop(0)
 
     def eat(self, food):
         del food
         self.score += 1
         print('Score:', self.score)
+        
                 
 class Game(arcade.Window):
     def __init__(self):
