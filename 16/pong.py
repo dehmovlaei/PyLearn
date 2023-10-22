c960f0900b4cca47d33a20279d2d9ef11f2bced4
dehmovlaei <db.dehmovlaei@gmail.com>
2023-10-22 22:50:36 +0330

    10222023:2250

--- 16/pong.py
+++ 16/pong.py
@@ -10,7 +10,7 @@ class Ball(arcade.Sprite):
         self.radius = 15
         self.change_x = random.choice([-1, 1])
         self.change_y = random.choice([-1, 1])
-        self.speed = 3
+        self.speed = 5
         self.width = self.radius * 2
         self.height = self.radius * 2
 
@@ -35,8 +35,19 @@ class Rocket(arcade.Sprite):
         self.speed = 4
         self.score = 0
     
-    def move(self):
-        ...
+    def move(self, game):
+        if game.ball.center_x > game.width//2:
+            
+            if self.center_y > game.ball.center_y:
+                self.change_y = -1
+            if self.center_y < game.ball.center_y:
+                self.change_y = 1
+            self.center_y += self.change_y * self.speed
+
+            if self.center_y < 60:
+                self.center_y = 60
+            if self.center_y > game.height - 60:
+                self.center_y =  game.height - 60
 
     def draw(self):
         arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width,
@@ -48,7 +59,11 @@ class Game(arcade.Window):
         arcade.set_background_color(arcade.color.DARK_GREEN)
         self.player1 = Rocket(40, self.height//2, arcade.color.RED, 'dehmovlaei')
         self.player2 = Rocket(self.width-40, self.height//2, arcade.color.CYAN, 'CPU')
+        self.sprite_list = arcade.SpriteList()
         self.ball = Ball(self)
+
+        self.sprite_list.append(self.player1)
+        self.sprite_list.append(self.player2)
 
     def on_draw(self):
         arcade.start_render()
@@ -63,19 +78,32 @@ class Game(arcade.Window):
         arcade.finish_render
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
-        #self.player1.center_x = x
-        if self.player1.height-11 < y < self.height - self.player1.height+11:
+        
+        if self.player1.height-11 < y < self.height-self.player1.height+11:
             self.player1.center_y = y
 
     def on_update(self, delta_time: float):
         self.ball.move()
+        self.player2.move(self)
         
         if self.ball.center_y < 30 or self.ball.center_y > self.height-30:
             self.ball.change_y *= -1
 
-        if arcade.check_for_collision(self.player1, self.ball) or arcade.check_for_collision(self.player2, self.ball):
+        if arcade.check_for_collision_with_list(self.ball, self.sprite_list):
             self.ball.change_x *= -1
-        
+
+        if self.ball.center_x < 0:
+            print('GOAL !!!')
+            self.player2.score += 1
+            del self.ball
+            self.ball = Ball(self)
+
+        if self.ball.center_x > self.width:
+            print('GOAL !!!')
+            self.player1.score += 1
+            del self.ball
+            self.ball = Ball(self)
+
 if __name__ == '__main__':
      game = Game()
      arcade.run()
