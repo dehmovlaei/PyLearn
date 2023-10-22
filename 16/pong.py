072e0d495db9fefa2fa8f4e542854e5b92cf4a3d
dehmovlaei <db.dehmovlaei@gmail.com>
2023-10-22 20:47:22 +0330

    10222023:2047

--- 16/pong.py
+++ 16/pong.py
@@ -10,7 +10,9 @@ class Ball(arcade.Sprite):
         self.radius = 15
         self.change_x = random.choice([-1, 1])
         self.change_y = random.choice([-1, 1])
-        self.speed = 4
+        self.speed = 3
+        self.width = self.radius * 2
+        self.height = self.radius * 2
 
     def move(self):
         self.center_x += self.change_x * self.speed
@@ -42,7 +44,7 @@ class Rocket(arcade.Sprite):
 
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__(width=800, height=500, title='Pong 2023 🏓🏓')
+        super().__init__(width=800, height=500, title='Pong 2023 🏓🏓', center_window=True)
         arcade.set_background_color(arcade.color.DARK_GREEN)
         self.player1 = Rocket(40, self.height//2, arcade.color.RED, 'dehmovlaei')
         self.player2 = Rocket(self.width-40, self.height//2, arcade.color.CYAN, 'CPU')
@@ -66,8 +68,14 @@ class Game(arcade.Window):
             self.player1.center_y = y
 
     def on_update(self, delta_time: float):
-        self.ball.move()        
+        self.ball.move()
+        
+        if self.ball.center_y < 30 or self.ball.center_y > self.height-30:
+            self.ball.change_y *= -1
 
+        if arcade.check_for_collision(self.player1, self.ball) or arcade.check_for_collision(self.player2, self.ball):
+            self.ball.change_x *= -1
+        
 if __name__ == '__main__':
      game = Game()
      arcade.run()
