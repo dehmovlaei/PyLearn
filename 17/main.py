14a7c0454b37b4e0558866965a924c1a7ce22950
dehmovlaei <db.dehmovlaei@gmail.com>
2023-11-26 23:13:24 +0330

    23:13

--- 17/main.py
+++ 17/main.py
@@ -1,2 +1,23 @@
 from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
+
+def sum():
+    global a
+    a = int(my_window.lineEdit.text())
+    my_window.lineEdit.setText('')
+
+def result():
+    b = int(my_window.lineEdit.text())
+    c = a + b
+    my_window.lineEdit.setText(str(c))
+
+
+loader = QUiLoader()
+app = QApplication([])
+my_window = loader.load('mainwindow.ui')
+my_window.show()
+
+my_window.btnSum.clicked.connect(sum)
+my_window.btnEqual.clicked.connect(result)
+
+app.exec()
