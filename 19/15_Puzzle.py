2382677b7de790ab89343c60135a0a0d2f01a438
dehmovlaei <db.dehmovlaei@gmail.com>
2023-12-31 21:26:40 +0330

    2126

---
+++ 19/15_Puzzle.py
@@ -0,0 +1,16 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from ui_mainwindow import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+app = QApplication(sys.argv)
+
+main_window = MainWindow()
+main_window.show()
+
+app.exec()
---
+++ 19/README.md
@@ -0,0 +1,8 @@
+# PyLearn Assignments
+## train some course and solved assignments
+
+---
+| File      | Description                            |
+|-----------|----------------------------------------|
+| TicTacToe | simple Tic Tac Toe Designed by PySide6 |
+![concentric](../18/TicTacToe.png)
