028f7b79ecf9c63b84a635cfb0a30b72009e2695
dehmovlaei <db.dehmovlaei@gmail.com>
2024-01-07 12:46:35 +0330

    1246

--- 19/15_Puzzle.py
+++ 19/15_Puzzle.py
@@ -1,7 +1,8 @@
 import sys
 import random
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from ui_mainwindow import Ui_MainWindow
+from functools import partial
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -19,7 +20,35 @@ class MainWindow(QMainWindow):
         for i in range(4):
             for j in range(4):
                 self.buttons[i][j].setText(str(my_list[index]))
+                self.buttons[i][j].clicked.connect(partial(self.play, i, j))
+                if my_list[index] == 16:
+                    self.buttons[i][j].setVisible(False)
+                    self.empty_i = i
+                    self.empty_j = j
                 index += 1
+
+    def play(self,i, j):
+        if i == self.empty_i and abs(j - self.empty_j) == 1 or j == self.empty_j and abs(i - self.empty_i) == 1:
+            self.buttons[self.empty_i][self.empty_j].setText(self.buttons[i][j].text())
+            self.buttons[i][j].setText("16")
+
+            self.buttons[self.empty_i][self.empty_j].setVisible(True)
+            self.buttons[i][j].setVisible(False)
+            self.empty_i = i
+            self.empty_j = j
+        if self.check_win() == True:
+            msgBox = QMessageBox()
+            msgBox.setText("YOU WIN ❤️")
+            msgBox.exec_()
+
+    def check_win(self):
+        index = 1
+        for i in range(4):
+            for j in range(4):
+                if int(self.buttons[i][j].text()) != index:
+                    return False
+                index += 1
+        return True
 
 app = QApplication(sys.argv)
 
