a460cbee2338842ff00de53a87e634834f6a58f7
dehmovlaei <db.dehmovlaei@gmail.com>
2024-03-05 16:39:24 +0330

    20240503

--- 23/main.py
+++ 23/main.py
@@ -3,6 +3,7 @@ import random
 from functools import partial
 from sudoku import Sudoku
 from PySide6.QtWidgets import *
+from PySide6.QtCore import *
 from mainwindow import Ui_MainWindow
 
 
@@ -17,8 +18,14 @@ class MainWindow(QMainWindow):
         for i in range(9):
             for j in range(9):
                 new_cell = QLineEdit()
+                new_cell.setFixedWidth(69)
+                new_cell.setFixedHeight(69)
+                new_cell.setFont("Adobe Arabic")
+                new_cell.setStyleSheet("font-weight:bold; font-size:69px; color:rgb(25, 42, 50)")
+                new_cell.setAlignment(Qt.AlignCenter)
                 self.ui.grid_layout.addWidget(new_cell, i, j)
-                new_cell.textChanged.connect(partial(self.validation, i, j))
+                item_index = self.ui.grid_layout.indexOf(new_cell)
+                new_cell.textChanged.connect(partial(self.validation, i, j, item_index))
                 self.line_edits[i][j] = new_cell
         self.new_game()
 
@@ -32,6 +39,7 @@ class MainWindow(QMainWindow):
                     self.line_edits[i][j].setReadOnly(True)
                 else:
                     self.line_edits[i][j].setText("")
+        print(puzzle.board)
 
     def open_file(self):
         file_path = QFileDialog.getOpenFileName(self, 'Open file', './', '')[0]
@@ -54,14 +62,94 @@ class MainWindow(QMainWindow):
                 else:
                     self.line_edits[i][j].setText("")
 
+    def check(self, row, col, item_index):
+        if 0 <= row <= 2 and 0 <= col <= 2:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(3):
+                for j in range(3):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        self.ui.grid_layout.itemAt(item_index).widget().setStyleSheet("font-weight:bold; font-size:69px; color:rgb(255, 0, 0)")
+        elif 0 <= row <= 2 and 3 <= col <= 5:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(3):
+                for j in range(3, 6):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        print("✖️✖️✖️")
+        elif 0 <= row <= 2 and 6 <= col <= 8:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(3):
+                for j in range(6, 9):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        print("✖️✖️✖️")
+        elif 3 <= row <= 5 and 0 <= col <= 2:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(3, 6):
+                for j in range(3):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        print("✖️✖️✖️")
+        elif 3 <= row <= 5 and 3 <= col <= 5:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(3, 6):
+                for j in range(3, 6):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        print("✖️✖️✖️")
+        elif 3 <= row <= 5 and 6 <= col <= 8:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(3, 6):
+                for j in range(6, 9):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        print("✖️✖️✖️")
+        elif 6 <= row <= 8 and 0 <= col <= 2:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(6, 9):
+                for j in range(3):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        print("✖️✖️✖️")
+        elif 6 <= row <= 8 and 3 <= col<= 5:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(6, 9):
+                for j in range(3, 6):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        print("✖️✖️✖️")
+        elif 6 <= row <= 8 and 6 <= col <= 8:
+            number_1 = self.line_edits[row][col].text()
+            for i in range(6, 9):
+                for j in range(6, 9):
+                    number_2 = self.line_edits[i][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != row and j != col:
+                        print("✖️✖️✖️")
 
-   def check():
-       ...
+        for i in range(0, 9):
+            for j in range(0, 9):
+                number_1 = self.line_edits[i][j].text()
+                for k in range(1, 9):
+                    number_2 = self.line_edits[i][k].text()
+                    if number_1 == number_2 and number_1 != "" and j != k:
+                        print("✖️")
+                        return False
+        for j in range(0, 9):
+            for i in range(0, 9):
+                number_1 = self.line_edits[i][j].text()
+                for k in range(1, 9):
+                    number_2 = self.line_edits[k][j].text()
+                    if number_1 == number_2 and number_1 != "" and i != k:
+                        print("✖️")
+                        return False
 
-    def validation(self, i, j, text):
+
+    def validation(self, i, j, item_index, text):
         if text not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             self.line_edits[i][j].setText("")
-
+        self.ui.grid_layout.itemAt(item_index).widget().setStyleSheet("font-weight:bold; font-size:69px; color:rgb(25, 42, 50)")
+        self.check(i, j, item_index)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
