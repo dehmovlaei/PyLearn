4d54ab249482d3d6244b9c0dbe0899d5e901092f
dehmovlaei <db.dehmovlaei@gmail.com>
2024-07-19 00:02:11 +0330

    ver0.0

--- 27/wolf.py
+++ 27/wolf.py
@@ -3,4 +3,16 @@ import cv2
 
 image = cv2.imread("./res/wolf.jpg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-print(image.shape)
+cv2.imshow("row", image)
+rows, cols = image.shape
+
+threshold = 100
+for r in range(rows):
+    for c in range(cols):
+        if image[r, c] > threshold:
+            image[r, c] = 255
+        else:
+            image[r, c] = 0
+
+cv2.imshow("result", image)
+cv2.waitKey()
